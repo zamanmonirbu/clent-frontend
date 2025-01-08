965fcde51a0a5df5afa2f6106739dd4159b3f751
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2025-01-08 14:48:02 +0600

    fixed

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 
-// export const BASE_URL = "https://client-lna3.onrender.com";
+export const BASE_URL = "https://client-lna3.onrender.com";
 
 
 
